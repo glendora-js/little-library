e5c06f7cec16ee94d420ce189a0b00fd13b94b11
mochacat <cat.r.mocha@gmail.com>
2015-12-11 16:53:53 -0800

    Added fields to library schema

--- models/Library.js
+++ models/Library.js
@@ -1,18 +1,27 @@
 var mongoose = require('mongoose');
 
 var librarySchema = new mongoose.Schema({
+  library_id : { type: String , unique: true},
   name: { type: String },
-  photo_url: { type: String},
-  address: { type: String, unique: true },
+  story: { type: String },
+  photo_url: { type: String },
+  charter: { type: Number },
+  street: { type: String },
+  city: { type: String },
+  state: { type: String },
+  zip: { type: Number },
   coordinates: {
     type: [Number], //longitude, latitude
     index: '2d'
   },
-
+  email : { 
+    type: String,
+    unique: true,
+    lowercase: true 
+  }, 
   steward_name: { type: String},
   steward_id: { type: Number},
 
-  email: { type: String, unique: true, lowercase: true }
 });
 
 module.exports = mongoose.model('Library', librarySchema);
